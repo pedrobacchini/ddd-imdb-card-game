317224040c45bf5dd0320b0490de98fb8d9fe08b
pedrobacchini <pedrobacchini@outlook.com>
2022-12-03 21:17:02 -0300

    chore: configure spring boot project

--- infrastructure/src/main/java/com/github/pedrobacchini/infrastructure/Main.java
+++ infrastructure/src/main/java/com/github/pedrobacchini/infrastructure/Main.java
@@ -1,8 +1,15 @@
 package com.github.pedrobacchini.infrastructure;
 
+
+import com.github.pedrobacchini.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
-//    public static void main(String[] args) {
-//        System.out.println(new UseCase().execute());
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerConfig.class, args);
+    }
+
 }
---
+++ infrastructure/src/main/java/com/github/pedrobacchini/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,11 @@
+package com.github.pedrobacchini.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.pedrobacchini")
+public class WebServerConfig {
+
+
+}
